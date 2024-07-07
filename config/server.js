b02e1a7cc74e5d4a4acd9a8fb1ee56816b57ecb8
abhii30 <akumar300599@gmail.com>
2024-07-07 17:59:58 +0530

    added jwtToken

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: [process.env.APP_KEY_1 || 'default_key_1', process.env.APP_KEY_2 || 'default_key_2'],
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
